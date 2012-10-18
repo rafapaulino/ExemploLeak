bebe9ce80236cfd954cd373e5df00209ce3a94ca
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-10-18 11:36:25 -0300

    Initial Commit

---
+++ ExemploLeakTests/ExemploLeakTests.m
@@ -0,0 +1,32 @@
+//
+//  ExemploLeakTests.m
+//  ExemploLeakTests
+//
+//  Created by Rafael Brigagão Paulino on 18/10/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "ExemploLeakTests.h"
+
+@implementation ExemploLeakTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ExemploLeakTests");
+}
+
+@end
